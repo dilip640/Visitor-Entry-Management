4728ae25c1dbc74d52a6caccf73c6ac37a1edd8c
dilip640 <dilipsharma640@gmail.com>
2019-12-01 19:17:07 +0530

    Host update

--- entryapp/urls.py
+++ entryapp/urls.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
-from entryapp.views import home
+from entryapp.views import home, host
 
 urlpatterns=[
     path('', home,name='home'),
+    path('host/', host,name='host'),
 ]
--- entryapp/views.py
+++ entryapp/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from entryapp.forms import CheckInForm
+from entryapp.forms import CheckInForm, HostForm
 from entryapp.models import Host
 
 # Create your views here.
@@ -12,7 +12,22 @@ def home(request):
             host = Host.objects.all()
             if host.count():
                 form.save()
+                return render(request, 'entryapp/showinfo.html',
+                                    {'info': "Successfuly CheckIn!"})
             else:
                 return render(request, 'entryapp/showinfo.html',
                                     {'error': "Please register Host first!"})
     return render(request, 'entryapp/index.html', {'form': form})
+
+def host(request):
+    try:
+        host = Host.objects.all().first()
+        form = HostForm(request.POST or None, instance=host)
+    except:
+        form = HostForm(request.POST or None)
+    if(request.method == 'POST'):
+        if form.is_valid():
+            form.save()
+            return render(request, 'entryapp/showinfo.html',
+                                    {'info': "Host Successfuly Updated!"})
+    return render(request, 'entryapp/host.html', {'form': form})
